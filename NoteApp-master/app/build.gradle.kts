ae32201aa6fc3e0b3637558b555fca21c9778881
A1manbet <wu34389@gmail.com>
2024-08-05 23:34:28 +0600

    Initial commit

---
+++ NoteApp-master/app/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+    //Ksp
+    id("com.google.devtools.ksp")
+    //Firebase
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.noteapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.noteapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.databinding.runtime)
+    implementation(libs.firebase.messaging)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Nav
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    
+    //Lottie
+    val lottieVersion = "6.4.0"
+    implementation ("com.airbnb.android:lottie:$lottieVersion")
+    implementation ("com.google.code.gson:gson:2.8.8")
+
+    //Room
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion") //подключила ksp
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    //Google auth
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+
+    //Firestore
+    implementation("com.google.firebase:firebase-firestore")
+}
---
+++ NoteApp-master/app/src/main/java/com/example/noteapp/ui/fragment/note/NoteFragment.kt
@@ -0,0 +1,108 @@
+package com.example.noteapp.ui.fragment.note
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.InvalidationTracker
+import com.example.noteapp.App
+import com.example.noteapp.R
+import com.example.noteapp.data.model.NoteModel
+import com.example.noteapp.databinding.FragmentNoteBinding
+import com.example.noteapp.interfaces.OnClickItem
+import com.example.noteapp.ui.adapter.NoteAdapter
+import com.example.noteapp.ui.fragment.chat.ChatFragment
+
+class NoteFragment : Fragment(), OnClickItem {
+
+    private lateinit var binding: FragmentNoteBinding
+    private val noteAdapter = NoteAdapter(onClick = this, onLongClick = this@NoteFragment)
+    private var isGridLayout = false
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListener()
+        initialize()
+        getData()
+        setupDrawerNavigation()
+    }
+
+    private fun initialize() {
+        binding.rvNote.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = noteAdapter
+        }
+    }
+
+    private fun setupListener() = with(binding) {
+        btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_noteFragment_to_detailNoteFragment)
+        }
+        btnChangeLayout.setOnClickListener {
+            isGridLayout = !isGridLayout
+            val layoutManager = if (isGridLayout) {
+                GridLayoutManager(requireContext(), 2)
+            } else {
+                LinearLayoutManager(requireContext())
+            }
+            binding.rvNote.layoutManager = layoutManager
+            updateLayoutChangerIcon()
+        }
+    }
+    private fun updateLayoutChangerIcon() {
+        val iconResId = if (isGridLayout) {
+            R.drawable.ic_linear_layout
+        } else {
+            R.drawable.ic_grid_layout
+        }
+        binding.btnChangeLayout.setImageResource(iconResId)
+    }
+
+    private fun getData() {
+        App().getInstance()?.noteDao()?.getAll()?.observe(viewLifecycleOwner) {
+            noteAdapter.submitList(it)
+        }
+    }
+    override fun onLongClick(noteModel: NoteModel) {
+        val builder = AlertDialog.Builder(requireContext())
+        with(builder){
+            setTitle("Вы уверены, что хотите удалить?")
+            setPositiveButton("Да"){dialog, which->
+                App().getInstance()?.noteDao()?.deleteNote(noteModel)
+
+            }
+            setNegativeButton("Нет"){dialog, which->
+                dialog.cancel()
+
+            }
+            show()
+        }
+        builder.create()
+    }
+    override fun onClick(noteModel: NoteModel) {
+        val action = NoteFragmentDirections.actionNoteFragmentToDetailNoteFragment(noteModel.id)
+        findNavController().navigate(action)
+    }
+
+    private fun setupDrawerNavigation() {
+        binding.imgMenu.setOnClickListener {
+            val drawerLayout = requireActivity().findViewById<DrawerLayout>(R.id.drawer_layout)
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+}
